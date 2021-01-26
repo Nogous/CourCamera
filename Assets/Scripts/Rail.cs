57e1d5a574372d938e9bdd3d93a889fecfb94f46
François <dessartsfran@gmail.com>
2021-01-26 11:22:49 +0100

    rail script

---
+++ Assets/Scripts/Rail.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rail : MonoBehaviour
+{
+    public bool isLoop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DrawGizmos(Color color)
+    {
+        Gizmos.color = color;
+    }
+}
