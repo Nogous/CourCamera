eb2f4cb7bb51b63e402bb319f0550191583b0665
Camille <camille.lavarde@gmail.com>
2021-01-26 10:02:20 +0100

    view

---
+++ Assets/Scripts/FixedView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedView : AView
+{
+    [Range(0, 360)]
+    public float yaw = 0f;
+
+    [Range(-90, 90)]
+    public float pitch = 0f;
+
+    [Range(-180, 180)]
+    public float roll = 0f;
+
+    [Range(1, 179)]
+    public float fov = 60;
+
+    public override CameraConfiguration GetConfiguration()
+    {
+        CameraConfiguration config = new CameraConfiguration();
+        config.yaw = yaw;
+        config.pitch = pitch;
+        config.roll = roll;
+        config.pivot = transform.position;
+        config.distance = 0;
+        config.fov = fov;
+
+        return config;
+    }
+}
