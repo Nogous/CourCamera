a519307b5a517c90b1abfca34f60fb49e415efba
Camille <camille.lavarde@gmail.com>
2021-01-26 11:28:24 +0100

    mieux

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -39,54 +39,59 @@ public class CameraController : MonoBehaviour
     private void Update()
     {
         CameraConfiguration config = new CameraConfiguration();
-        float weight = 0f;
-        bool weightZero;
 
-        foreach (AView view in activeViews)
+        if (activeViews.Count <= 0)
         {
-            weight += view.weight;
-        }
+            float weight = 0f;
+            bool weightZero;
 
-        if (weight == 0)
-            weightZero = true;
-        else
-            weightZero = false;
-
-        if (weightZero)
-        {
             foreach (AView view in activeViews)
             {
-                CameraConfiguration tmpConfig = view.GetConfiguration();
-                config.yaw += tmpConfig.yaw;
-                config.pitch += tmpConfig.pitch;
-                config.roll += tmpConfig.roll;
-                config.fov += tmpConfig.fov;
-                config.pivot += tmpConfig.pivot;
-                config.distance += tmpConfig.distance;
-
-                weight++;
+                weight += view.weight;
             }
-        }
-        else
-        {
-            foreach (AView view in activeViews)
+
+            if (weight == 0)
+                weightZero = true;
+            else
+                weightZero = false;
+
+            if (weightZero)
             {
-                CameraConfiguration tmpConfig = view.GetConfiguration();
-                config.yaw += tmpConfig.yaw * view.weight;
-                config.pitch += tmpConfig.pitch * view.weight;
-                config.roll += tmpConfig.roll * view.weight;
-                config.fov += tmpConfig.fov * view.weight;
-                config.pivot += tmpConfig.pivot * view.weight;
-                config.distance += tmpConfig.distance * view.weight;
+                foreach (AView view in activeViews)
+                {
+                    CameraConfiguration tmpConfig = view.GetConfiguration();
+                    config.yaw += tmpConfig.yaw;
+                    config.pitch += tmpConfig.pitch;
+                    config.roll += tmpConfig.roll;
+                    config.fov += tmpConfig.fov;
+                    config.pivot += tmpConfig.pivot;
+                    config.distance += tmpConfig.distance;
+
+                    weight++;
+                }
             }
+            else
+            {
+                foreach (AView view in activeViews)
+                {
+                    CameraConfiguration tmpConfig = view.GetConfiguration();
+                    config.yaw += tmpConfig.yaw * view.weight;
+                    config.pitch += tmpConfig.pitch * view.weight;
+                    config.roll += tmpConfig.roll * view.weight;
+                    config.fov += tmpConfig.fov * view.weight;
+                    config.pivot += tmpConfig.pivot * view.weight;
+                    config.distance += tmpConfig.distance * view.weight;
+                }
+            }
+
+            config.yaw /= weight;
+            config.pitch /= weight;
+            config.roll /= weight;
+            config.fov /= weight;
+            config.pivot /= weight;
+            config.distance /= weight;
         }
 
-        config.yaw /= weight;
-        config.pitch /= weight;
-        config.roll /= weight;
-        config.fov /= weight;
-        config.pivot /= weight;
-        config.distance /= weight;
 
         transform.rotation = config.GetRotation();
         transform.position = config.GetPosition();
