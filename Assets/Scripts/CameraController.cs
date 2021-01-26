b575e9aa1b91fabbeba113eb4ebcff82ac1a04b4
François <dessartsfran@gmail.com>
2021-01-26 10:19:57 +0100

    remove old code

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -19,73 +19,10 @@ public class CameraController : MonoBehaviour
         }
     }
 
-    #region oldLerp
+    public CameraConfiguration configCommon;
+    public CameraConfiguration configTarget;
 
-    public CameraConfiguration configA;
-    public CameraConfiguration configB;
-
-    [Range(0, 360)]
-    private float yaw;
-
-    [Range(-90, 90)]
-    private float pitch;
-
-    [Range(-180, 180)]
-    private float roll;
-
-    private Vector3 pivot;
-
-    private float distance;
-
-    /*
-    [Space(1)]
-    [Header("Lerp")]
-    public bool useLerpValue = true;
-
-    [Range(0,1)]
-    public float lerpPotencial = 0f;
-
-    public float lerpSpeed = 0f;
-    private bool positif = true;
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (positif)
-        {
-            if (lerpPotencial>1)
-            {
-                positif = !positif;
-            }
-            lerpPotencial += lerpSpeed * Time.deltaTime;
-        }
-        else
-        {
-            if (lerpPotencial < 0)
-            {
-                positif = !positif;
-            }
-            lerpPotencial -= lerpSpeed * Time.deltaTime;
-        }
-
-        if (useLerpValue)
-        {
-            yaw = Mathf.Lerp(configA.yaw, configB.yaw, lerpPotencial);
-            pitch = Mathf.Lerp(configA.pitch, configB.pitch, lerpPotencial);
-            roll = Mathf.Lerp(configA.roll, configB.roll, lerpPotencial);
-            pivot = Vector3.Lerp(configA.pivot, configB.pivot, lerpPotencial);
-            distance = Mathf.Lerp(configA.distance, configB.distance, lerpPotencial);
-            fov = Mathf.Lerp(configA.fov, configB.fov, lerpPotencial);
-        }
-
-        Quaternion orientation = Quaternion.Euler(pitch, yaw, roll);
-        transform.rotation = orientation;
-        Vector3 offset = orientation * (Vector3.back * distance);
-        transform.position = pivot + offset;
-    }
-    */
-
-    #endregion
+    public float cameraSpeed = 0.1f;
 
     private List<AView> activeViews = new List<AView>();
 
@@ -97,5 +34,10 @@ public class CameraController : MonoBehaviour
     public void RemoveView(AView view)
     {
         activeViews.Remove(view);
+    }
+
+    private void Update()
+    {
+        
     }
 }
