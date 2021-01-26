92a821529deb9939158196be88a0f05a24b8bfaf
Camille <camille.lavarde@gmail.com>
2021-01-26 09:45:00 +0100

    GetRotation + GetPosition

--- Assets/Scripts/CameraConfiguration.cs
+++ Assets/Scripts/CameraConfiguration.cs
@@ -19,4 +19,15 @@ public class CameraConfiguration : MonoBehaviour
 
     [Range(1,179)]
     public float fov = 60;
+
+    public Quaternion GetRotation()
+    {
+        return Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    public Vector3 GetPosition()
+    {
+        Vector3 offset = GetRotation() * (Vector3.back * distance);
+        return pivot + offset;
+    }
 }
