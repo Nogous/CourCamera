0c21eb1ced531f4e2a492fd49a93b7f87fb6b4b8
Camille <camille.lavarde@gmail.com>
2021-01-29 10:59:23 +0100

    1.4

---
+++ Assets/Scripts/TriggeredViewVolume.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class TriggeredViewVolume : AViewVolume
+{
+    GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        view.SetActive(true);   
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        view.SetActive(false);
+    }
+}
