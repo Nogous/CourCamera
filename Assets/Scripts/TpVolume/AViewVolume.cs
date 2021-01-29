cfdc574bba786905f1ead98fd8d27d8c3909acc9
François <dessartsfran@gmail.com>
2021-01-29 10:28:10 +0100

    Tp2 Start

---
+++ Assets/Scripts/TpVolume/AViewVolume.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AViewVolume : MonoBehaviour
+{
+    public int priority = 0;
+    public AView view;
+
+    public virtual float ComputeSelfWeight()
+    {
+        return 1.0f;
+    }
+}
---
+++ Assets/Scripts/TpVolume/ViewVolumeBlender.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewVolumeBlender : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static ViewVolumeBlender instance;
+
+    private void Awake()
+    {
+        if(instance == null){
+
+            instance = this;
+
+        } else
+        {
+            Destroy(this);
+        }
+    }
+
+    private List<AViewVolume> ActiveViewVolumes = new List<AViewVolume>();
+    private Dictionary<AView, List<AViewVolume>> VolumesPerViews = new Dictionary<AView, List<AViewVolume>>();
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddVolume(AViewVolume viewVolume)
+    {
+        ActiveViewVolumes.Add(viewVolume);
+
+        if (!VolumesPerViews.ContainsKey(viewVolume.view))
+        {
+            VolumesPerViews.Add(viewVolume.view, new List<AViewVolume>());
+            viewVolume.view.SetActive(true);
+        }
+
+        foreach(var i in VolumesPerViews)
+        {
+            if(i.Key == viewVolume.view)
+            {
+                i.Value.Add(viewVolume);
+            }
+        }
+        
+    }
+    public void RemoveVolume(AViewVolume viewVolume)
+    {
+        ActiveViewVolumes.Remove(viewVolume);
+    }
+}
