c9ced0a433673ce6f040482329a432f8130769ea
François <dessartsfran@gmail.com>
2021-01-31 15:27:20 +0100

    respawn

---
+++ Assets/Scripts/TpVolume/respawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+    public Vector3 respawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerController>())
+        other.transform.position = respawnPoint;
+    }
+}
