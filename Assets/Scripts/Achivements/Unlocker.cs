7432d4f96e4064ae90cd74c8f985de0c62de0219
Camille <camille.lavarde@gmail.com>
2021-01-31 17:06:51 +0100

    achivements

---
+++ Assets/Scripts/Achivements/Unlocker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unlocker : MonoBehaviour
+{
+    private AchivementChecker _checker;
+    private int _id = 0;
+
+    public void SetAchivementChecker(AchivementChecker checker, int id)
+    {
+        _checker = checker;
+        _id = id;
+    }
+
+    public void SetEndColor()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _checker.UnlockSwitch(this, _id);
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+}
